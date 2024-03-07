a00b37e43696ac3c642928cf1dbd44cf6827bceb
vaghinak767 <147193790+vaghinak767@users.noreply.github.com>
2024-03-07 22:59:06 +0400

    Update double_linked_list.h

--- double_linked_list.h
+++ double_linked_list.h
@@ -1,7 +1,13 @@
 #include "node.h"
-
 class List
 {
+public:
+	using value_type = int;
+    using reference = value_type&;
+    using const_reference = const value_type&;
+    using size_type = std::size_t;
+    using pointer = value_type*;
+    using const_pointer = const value_type*;
 public:
     List();
     ~List();
@@ -27,6 +33,167 @@ public:
     int operator[](int index);
     bool operator==(const List& ob)const;
     const List operator+(const List& ob);
+private:
+	class base_iterator
+    {
+        friend List;
+    public:
+        ~base_iterator();
+        bool operator==(const base_iterator& rhv) const;
+        bool operator!=(const base_iterator& rhv) const;
+    protected:
+        explicit base_iterator(Node* ptr);
+    protected:
+        Node* ptr = nullptr;
+    };
+	public:
+		class const_iterator : public base_iterator
+		{
+			friend List;
+			public:
+				const_iterator(const base_iterator& rhv);
+				const_iterator(base_iterator&& rhv);	
+				const const_iterator& operator=(const base_iterator& rhv);
+				const const_iterator& operator=(base_iterator&& rhv);
+				const_reference operator*() const;
+				const_pointer operator->() const;
+				const const_iterator& operator++();
+				const const_iterator operator++(value_type);
+				const const_iterator& operator--();
+				const const_iterator operator--(value_type);
+			protected:
+				explicit const_iterator(Node* ptr);
+		};
+	public:
+		class iterator : public const_iterator
+		{
+			friend List;
+		public:
+			iterator(const base_iterator& rhv);
+			iterator(base_iterator&& rhv);
+			reference operator*();
+			pointer operator->();
+			const iterator& operator=(const base_iterator& rhv);
+			const iterator& operator=(base_iterator&& rhv);
+		protected:
+			explicit iterator(Node* ptr);
+		};
+	public:
+    class const_reverse_iterator : public base_iterator
+    {
+        friend List;
+    public:
+        const_reverse_iterator(const base_iterator& rhv);
+        const_reverse_iterator(base_iterator&& rhv);
+        const const_reverse_iterator& operator=(const base_iterator& rhv);
+        const const_reverse_iterator& operator=(base_iterator&& rhv);
+        const_reference operator*() const;
+        const_pointer operator->() const;
+        const const_reverse_iterator& operator++();
+        const const_reverse_iterator operator++(value_type);
+        const const_reverse_iterator& operator--();
+        const const_reverse_iterator operator--(value_type);
+    protected:
+        explicit const_reverse_iterator(Node* ptr);
+    };
+	public:
+    class reverse_iterator : public const_reverse_iterator
+    {
+        friend List;
+    public:
+        reverse_iterator(const base_iterator& rhv);
+        reverse_iterator(base_iterator&& rhv);
+        reference operator*();
+        pointer operator->();
+		const reverse_iterator& operator=(const base_iterator& rhv);
+        const reverse_iterator& operator=(base_iterator&& rhv);
+    protected:
+        explicit reverse_iterator(Node* ptr);
+    };
+	public:
+	class const_asc_iterator :public base_iterator
+    {
+        friend List;
+    public:
+        const_asc_iterator(const base_iterator& rhv);
+        const_asc_iterator(base_iterator&& rhv);
+        const const_asc_iterator& operator=(const base_iterator& rhv);
+        const const_asc_iterator& operator=(base_iterator&& rhv);
+        const_reference operator*() const;
+        const_pointer operator->() const;
+        const const_asc_iterator& operator++();
+        const const_asc_iterator operator++(value_type);
+        const const_asc_iterator& operator--();
+        const const_asc_iterator operator--(value_type);
+    protected:
+        explicit const_asc_iterator(Node* ptr);
+    };
+	public:
+		class asc_iterator : public const_asc_iterator
+		{
+			friend List;
+		public:
+			asc_iterator(const base_iterator& rhv);
+			asc_iterator(base_iterator&& rhv);
+			reference operator*();
+			pointer operator->();
+			const asc_iterator& operator=(const base_iterator& rhv);
+			const asc_iterator& operator=(base_iterator&& rhv);
+		protected:
+			explicit asc_iterator(Node* ptr);
+		};
+public:
+	class const_desc_iterator : public base_iterator
+	{
+		friend List;
+	public:
+		const_desc_iterator(const base_iterator& rhv);
+		const_desc_iterator(base_iterator&& rhv);
+		const const_desc_iterator& operator=(const base_iterator& rhv);
+		const const_desc_iterator& operator=(base_iterator&& rhv);
+		const_reference operator*() const;
+		const_pointer operator->() const;
+		const const_desc_iterator& operator++();
+		const const_desc_iterator operator++(value_type);
+		const const_desc_iterator& operator--();
+		const const_desc_iterator operator--(value_type);
+	protected:
+		explicit const_desc_iterator(Node* ptr);
+	};
+public:
+    class desc_iterator : public const_desc_iterator
+    {
+        friend List;
+    public:
+        desc_iterator(const base_iterator& rhv);
+        desc_iterator(base_iterator&& rhv);
+        reference operator*();
+        pointer operator->();
+        const desc_iterator& operator=(const base_iterator& rhv);
+        const desc_iterator& operator=(base_iterator&& rhv);
+    protected:
+        explicit desc_iterator(Node* ptr);
+    };
+public:
+    class const_multi_iterator : public base_iterator
+    {
+        friend List;
+    public:
+        const_multi_iterator(const base_iterator& rhv);
+        const_multi_iterator(base_iterator&& rhv);
+        const const_multi_iterator& operator=(const base_iterator& rhv);
+        const const_multi_iterator& operator=(base_iterator&& rhv);
+        const_reference operator*() const;
+        const_pointer operator->() const;
+        const const_multi_iterator& operator++();
+        const const_multi_iterator operator++(value_type); //O(1)
+        const const_multi_iterator& operator--(); //O(1)
+        const const_multi_iterator operator--(value_type);
+        void chmod(); //O(1)
+    protected:
+        explicit const_multi_iterator(Node* ptr); //O(1)
+        bool mode = true;
+    };
 private:
     Node *head = nullptr;
     Node *tail = nullptr;
